5bb8c471ee54aee4504af5a5413aaa0036e7fdf1
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-17 10:56:14 +0530

    Send an email wit news titles and description

--- venu/main.py
+++ venu/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "4287e57336d7459fbd8b64d3dce7ddb9"
 url = "https://newsapi.org/v2/everything?q=tesla&"\
@@ -9,7 +10,15 @@ url = "https://newsapi.org/v2/everything?q=tesla&"\
 request = requests.get(url)
 # get a dictionary with data
 content = request.json()
+
+
 # Access the article titles and description
+body = " "
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"]  is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
+    
+    
